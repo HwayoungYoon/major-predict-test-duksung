31014eba09a99c075ec48596c9878e26a630a720
HwayoungYoon <54611046+HwayoungYoon@users.noreply.github.com>
2020-06-25 14:14:09 +0900

    Add files via upload

---
+++ DB/db_data_student.sql
@@ -0,0 +1,13 @@
+/* STUDENT table column 설명
+STUDENT_ID : 학생별 id(학번)
+COLLEGE : 단과대명
+MAJOR_NAME : 전공명
+CREDIT_1 : 1학기 학점
+CREDIT_2 : 2학기 학점
+GPA_1 : 1학기 평점
+GPA_2 : 2학기 평점
+EXTRA_ACTIVITY : 비교과 점수
+COMPLETE_MAJOR : 전공이수여부(0은 이수 안함, 400은 이수 함) */
+
+/* 데이터 입력 */
+INSERT INTO student VALUES(20200001, '글로벌융합', '국어국문학', 18, 18, 4.5, 4.5, 300, 400) ;
